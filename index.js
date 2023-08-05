f9e3cc2ad621c30710fe7083dcf0d8c5a147189c
Yash Vardhan Singh <yashvardhansingh7465@gmail.com>
2023-08-05 11:41:54 +0530

    Revising MERN stack

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = 3000;
-
-// app.use(express.static(path.join(__dirname, 'static')));
-app.use('/', require(path.join(__dirname, 'routes/blog.js')))
-
-// app.get
-
-app.listen(port, (err) => {
-    if (err) console.log('there is an error.');
-    console.log(`Blog App is listening on server: http://localhost:${port}`);
+/*
+const { EventEmitter } = require('events');
+const eventEmittor = new EventEmitter();
+eventEmittor.on('lunch', ()=> {
+    console.log('yum');
 })
+eventEmittor.on('cool', ()=> {
+    console.log('damn');
+})
+
+eventEmittor.emit('lunch');
+eventEmittor.emit('cool');
+*/
+
+const express = require('express');
 
