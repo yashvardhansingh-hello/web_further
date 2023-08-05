3bad5915c76033c6fe221324d4cc4e8bda43a788
Yash Vardhan Singh <yashvardhansingh7465@gmail.com>
2023-08-05 12:20:26 +0530

    Rivision Complete

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-/*
-const { EventEmitter } = require('events');
-const eventEmittor = new EventEmitter();
-eventEmittor.on('lunch', ()=> {
-    console.log('yum');
-})
-eventEmittor.on('cool', ()=> {
-    console.log('damn');
-})
-
-eventEmittor.emit('lunch');
-eventEmittor.emit('cool');
-*/
-
 const express = require('express');
+const app = express();
+const PORT = 3000
 
+const {readFile, readFileSync} = require('fs');
+
+app.listen(PORT, (err)=>{
+    if(err) console.log(err);
+    console.log('App available on port', PORT);
+})
+
+app.get('/', (request, response) => {
+    readFile('./home.html', 'utf8', (err, html) =>{
+        if(err){
+            response.status(500).send('sorry out of order');
+        }
+        response.send(html);
+    })
+});
